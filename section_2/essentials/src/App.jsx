ac733c2ab17bfb55a3b843fecb659dc0f77d737d
yonghowfung <yhfung940217@gmail.com>
2024-02-04 18:15:53 +0100

    create a component and use in App.jsx

--- section_2/essentials/src/App.jsx
+++ section_2/essentials/src/App.jsx
@@ -1,19 +1,14 @@
-function App() {
+import Header from "./components/Header";
+
+const App = () => {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ section_2/essentials/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import reactImage from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+const Header = () => {
+  const genRandomInt = (max) => {
+    return Math.floor(Math.random() * (max + 1));
+  };
+
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+};
+
+export default Header;
