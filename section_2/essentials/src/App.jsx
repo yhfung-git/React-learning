b4a1b840a95faab91af7373f11d63b89f73a5958
yonghowfung <yhfung940217@gmail.com>
2024-02-04 20:05:10 +0100

    base

--- section_2/essentials/src/App.jsx
+++ section_2/essentials/src/App.jsx
@@ -1,11 +1,18 @@
 import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
+import { CORE_CONCEPTS } from "./data";
 
 const App = () => {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept concepts={CORE_CONCEPTS} />
+          </ul>
+        </section>
       </main>
     </div>
   );
