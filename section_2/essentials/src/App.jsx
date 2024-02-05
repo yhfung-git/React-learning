f93bfaa2ed7ec02549d72bd8fb6c4ba83dc3ea3b
yonghowfung <yhfung940217@gmail.com>
2024-02-05 18:51:47 +0100

    use TabButton component

--- section_2/essentials/src/App.jsx
+++ section_2/essentials/src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 const App = () => {
+  const [content, setContent] = useState("");
+
+  const selectHandler = (selectedButton) => {
+    setContent(selectedButton);
+  };
+
   return (
     <div>
       <Header />
@@ -12,6 +21,18 @@ const App = () => {
           <ul>
             <CoreConcept concepts={CORE_CONCEPTS} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => selectHandler("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => selectHandler("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => selectHandler("props")}>Props</TabButton>
+            <TabButton onSelect={() => selectHandler("state")}>State</TabButton>
+          </menu>
+          {content}
         </section>
       </main>
     </div>
