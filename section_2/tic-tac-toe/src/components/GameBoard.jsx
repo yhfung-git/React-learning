35e7452ecf8a32e666944d895758956ed3e29204
yonghowfung <yhfung940217@gmail.com>
2024-02-06 22:49:38 +0100

    GameBoard component for showing the game board

---
+++ section_2/tic-tac-toe/src/components/GameBoard.jsx
@@ -0,0 +1,36 @@
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+const GameBoard = ({ onSelectSquare, turns }) => {
+  let gameBoard = [...initialGameBoard];
+
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
