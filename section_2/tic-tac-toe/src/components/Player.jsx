d3635f2f7acbc3f2287068d9d8ffe4b7f4b7472d
yonghowfung <yhfung940217@gmail.com>
2024-02-06 21:23:08 +0100

    pass isActive to Player component

--- section_2/tic-tac-toe/src/components/Player.jsx
+++ section_2/tic-tac-toe/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Player = ({ initialName, symbol }) => {
+const Player = ({ initialName, symbol, isActive }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
 
@@ -19,7 +19,7 @@ const Player = ({ initialName, symbol }) => {
   );
 
   return (
-    <li>
+    <li className={isActive ? "active" : ""}>
       <span className="player">
         <span className="player-name">{renderedPlayerName}</span>
         <span className="player-symbol">{symbol}</span>
