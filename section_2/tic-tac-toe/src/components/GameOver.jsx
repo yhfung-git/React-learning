afd513cb9be6126befc6c56a9ac96715928c6200
yonghowfung <yhfung940217@gmail.com>
2024-02-07 21:24:03 +0100

    changed showing text

--- section_2/tic-tac-toe/src/components/GameOver.jsx
+++ section_2/tic-tac-toe/src/components/GameOver.jsx
@@ -2,7 +2,7 @@ const GameOver = ({ winner, onRestart }) => {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
-      <p>{winner ? `Player ${winner} won!` : "It's a draw!"}</p>
+      <p>{winner ? `${winner} won!` : "It's a draw!"}</p>
       <p>
         <button onClick={onRestart}>Rematch!</button>
       </p>
