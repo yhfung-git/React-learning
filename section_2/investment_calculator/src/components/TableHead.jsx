71bff60b1f01007ca1e5764d940af40a7aa71dbb
yonghowfung <yhfung940217@gmail.com>
2024-02-08 14:55:15 +0100

    table head

---
+++ section_2/investment_calculator/src/components/TableHead.jsx
@@ -0,0 +1,15 @@
+const TableHead = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Year</th>
+        <th>Investment Value</th>
+        <th>Interest (Year)</th>
+        <th>Total Interest</th>
+        <th>Invested Capital</th>
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHead;
