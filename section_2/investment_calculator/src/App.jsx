52fafd921f1ed0e7be042e3569cf55d07c70a9bd
yonghowfung <yhfung940217@gmail.com>
2024-02-08 16:44:36 +0100

    APP

--- section_2/investment_calculator/src/App.jsx
+++ section_2/investment_calculator/src/App.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Result from "./components/Result";
+
 const App = () => {
-  return <h1>Coming soon...</h1>;
+  const [values, setValues] = useState({
+    initialInvestment: 15000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const handleChange = (name, newValue) => {
+    setValues((preValues) => {
+      return { ...preValues, [name]: newValue };
+    });
+  };
+
+  const isValid = Object.values(values).every((value) => value > 0);
+
+  return (
+    <>
+      <Header />
+      <Form values={values} onChange={handleChange} />
+      {isValid && <Result values={values} />}
+      {!isValid && (
+        <p className="center">Please provide values for all fields.</p>
+      )}
+    </>
+  );
 };
 
 export default App;
