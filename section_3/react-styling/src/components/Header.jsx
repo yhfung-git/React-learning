9f886aeed40d2c7e0c740901311164248058b35c
yonghowfung <yhfung940217@gmail.com>
2024-02-08 18:39:49 +0100

    start react styling project

---
+++ section_3/react-styling/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
+    </header>
+  );
+};
+export default Header;
