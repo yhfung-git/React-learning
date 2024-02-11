50038696ed2b7f20a755a4da7bb792d7d9f930f0
yonghowfung <yhfung940217@gmail.com>
2024-02-11 13:48:15 +0100

    handle playerName

--- section_4/mini-game/src/components/Player.jsx
+++ section_4/mini-game/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState, useRef } from "react";
+
 const Player = () => {
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+
+  const handleClick = () => {
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>
+        Welcome{" "}
+        {enteredPlayerName?.length > 0 ? enteredPlayerName : "unknown entity"}
+      </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
