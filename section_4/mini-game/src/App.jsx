9ec0cb0f8423092964f6e340ba8473ddef1466bc
yonghowfung <yhfung940217@gmail.com>
2024-02-11 17:47:19 +0100

    use TimerChallenge component

--- section_4/mini-game/src/App.jsx
+++ section_4/mini-game/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from "./components/Player";
+import TimerChallenge from "./components/TimerChallenge";
 
 const App = () => {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Middle" targetTime={5} />
+        <TimerChallenge title="Difficult" targetTime={10} />
+        <TimerChallenge title="Expert" targetTime={15} />
+      </div>
     </>
   );
 };
