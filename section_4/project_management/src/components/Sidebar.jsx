129c566410bcab9beaa8208a54f29fc106cd977b
yonghowfung <yhfung940217@gmail.com>
2024-02-12 21:17:04 +0100

    show list projects added on sidebar

--- section_4/project_management/src/components/Sidebar.jsx
+++ section_4/project_management/src/components/Sidebar.jsx
@@ -1,9 +1,11 @@
 import Button from "./Button";
 
-const Sidebar = ({ onStartAddProject }) => {
+const Sidebar = ({ onStartAddProject, projects }) => {
   const asideClasses =
     "bg-stone-900 text-stone-50 rounded-tr-xl w-1/3 px-8 py-16 md:w-72";
   const titleClasses = "mb-8 uppercase font-bold md:text-xl text-stone-200";
+  const buttonClasses =
+    "w-full text-left px-2 py-1 my-1 capitalize rounded-sm text-stone-400 hover:text-stone-200 hover:bg-stone-800";
 
   return (
     <aside className={asideClasses}>
@@ -11,7 +13,13 @@ const Sidebar = ({ onStartAddProject }) => {
       <div>
         <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
-      <ul></ul>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className={buttonClasses}>{project.title}</button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 };
