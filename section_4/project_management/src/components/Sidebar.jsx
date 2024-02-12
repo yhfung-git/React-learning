ad675018e898aa747a8b13b0f9aae874a9c8b684
yonghowfung <yhfung940217@gmail.com>
2024-02-13 00:42:00 +0100

    dynamic css and pass onSelectProject function to pass the project id selected

--- section_4/project_management/src/components/Sidebar.jsx
+++ section_4/project_management/src/components/Sidebar.jsx
@@ -1,11 +1,14 @@
 import Button from "./Button";
 
-const Sidebar = ({ onStartAddProject, projects }) => {
+const Sidebar = ({
+  onStartAddProject,
+  projects,
+  onSelectProject,
+  selectedProjectId,
+}) => {
   const asideClasses =
     "bg-stone-900 text-stone-50 rounded-tr-xl w-1/3 px-8 py-16 md:w-72";
   const titleClasses = "mb-8 uppercase font-bold md:text-xl text-stone-200";
-  const buttonClasses =
-    "w-full text-left px-2 py-1 my-1 capitalize rounded-sm text-stone-400 hover:text-stone-200 hover:bg-stone-800";
 
   return (
     <aside className={asideClasses}>
@@ -14,11 +17,25 @@ const Sidebar = ({ onStartAddProject, projects }) => {
         <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
       <ul className="mt-8">
-        {projects.map((project) => (
-          <li key={project.id}>
-            <button className={buttonClasses}>{project.title}</button>
-          </li>
-        ))}
+        {projects.map((project) => {
+          let buttonClasses =
+            "w-full text-left px-2 py-1 my-1 capitalize rounded-sm hover:text-stone-200 hover:bg-stone-800";
+
+          project.id === selectedProjectId
+            ? (buttonClasses += " bg-stone-800 text-stone-200")
+            : (buttonClasses += " text-stone-400");
+
+          return (
+            <li key={project.id}>
+              <button
+                className={buttonClasses}
+                onClick={() => onSelectProject(project.id)}
+              >
+                {project.title}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
