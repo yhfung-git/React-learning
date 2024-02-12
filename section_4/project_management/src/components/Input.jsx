40234371de1a592b65e1757ca3cfb889214e7d36
yonghowfung <yhfung940217@gmail.com>
2024-02-12 19:25:39 +0100

    added css to textarea

--- section_4/project_management/src/components/Input.jsx
+++ section_4/project_management/src/components/Input.jsx
@@ -1,6 +1,9 @@
 const Input = ({ textarea, label, name, ...props }) => {
-  const inputCSS =
+  let inputClasses =
     "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
+  if (textarea) inputClasses += " min-h-20 max-h-52";
+
   return (
     <p className="flex flex-col gap-1 my-4">
       <label
@@ -10,9 +13,9 @@ const Input = ({ textarea, label, name, ...props }) => {
         {label}
       </label>
       {textarea ? (
-        <textarea {...props} className={inputCSS}></textarea>
+        <textarea {...props} className={inputClasses}></textarea>
       ) : (
-        <input {...props} className={inputCSS} />
+        <input {...props} className={inputClasses} />
       )}
     </p>
   );
