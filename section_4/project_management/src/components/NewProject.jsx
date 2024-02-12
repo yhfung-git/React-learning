4f338eaf3277a6a710203dec74601d8fe50cfe2a
yonghowfung <yhfung940217@gmail.com>
2024-02-12 21:05:05 +0100

    use useRef to handle input value for submission

--- section_4/project_management/src/components/NewProject.jsx
+++ section_4/project_management/src/components/NewProject.jsx
@@ -1,8 +1,26 @@
+import { useRef } from "react";
+
 import Input from "./Input";
 
-const NewProject = ({ onCancel }) => {
+const NewProject = ({ onCancel, onAddProject }) => {
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+  const dueDateRef = useRef();
+
   const buttonClasses =
     "px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950";
+
+  const handleSave = () => {
+    const title = titleRef.current.value;
+    const description = descriptionRef.current.value;
+    const dueDate = dueDateRef.current.value;
+
+    onAddProject({ title, description, dueDate });
+
+    titleRef.current.value = "";
+    descriptionRef.current.value = "";
+    dueDateRef.current.value = "";
+  };
 
   return (
     <div className="w-[35rem] my-16 mx-auto">
@@ -16,19 +34,34 @@ const NewProject = ({ onCancel }) => {
           </button>
         </li>
         <li>
-          <button className={buttonClasses}>Save</button>
+          <button className={buttonClasses} onClick={handleSave}>
+            Save
+          </button>
         </li>
       </menu>
       <div>
-        <Input type="text" name="title" id="title" label="Title" />
+        <Input
+          type="text"
+          name="title"
+          id="title"
+          label="Title"
+          ref={titleRef}
+        />
         <Input
           name="description"
           id="description"
           label="Description"
           rows="3"
           textarea
+          ref={descriptionRef}
         />
-        <Input type="date" name="date" id="date" label="Due Date" />
+        <Input
+          type="date"
+          name="date"
+          id="date"
+          label="Due Date"
+          ref={dueDateRef}
+        />
       </div>
     </div>
   );
