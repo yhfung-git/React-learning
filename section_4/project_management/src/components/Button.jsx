682f65db0c8f378866d33d33fd6523dfc45ff760
yonghowfung <yhfung940217@gmail.com>
2024-02-12 19:16:47 +0100

    changed classes name

--- section_4/project_management/src/components/Button.jsx
+++ section_4/project_management/src/components/Button.jsx
@@ -1,9 +1,9 @@
 const Button = ({ children, ...props }) => {
-  const buttonCSS =
+  const buttonClasses =
     "px-4 py-2 rounded-md text-xs md:text-base bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100";
 
   return (
-    <button className={buttonCSS} {...props}>
+    <button className={buttonClasses} {...props}>
       {children}
     </button>
   );
