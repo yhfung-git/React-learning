4305ba261337589c08bde86932e68b92fe046a80
yonghowfung <yhfung940217@gmail.com>
2024-02-12 19:36:22 +0100

    use Sidebar, NewProject and Homepage components

--- section_4/project_management/src/App.jsx
+++ section_4/project_management/src/App.jsx
@@ -1,8 +1,46 @@
+import { useState } from "react";
+
+import Sidebar from "./components/Sidebar";
+import NewProject from "./components/NewProject";
+import Homepage from "./components/Homepage";
+
 const App = () => {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  const handleCancelAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  };
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onCancel={handleCancelAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <Homepage onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="flex gap-8 mt-8 h-screen">
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
+    </main>
   );
 };
 
