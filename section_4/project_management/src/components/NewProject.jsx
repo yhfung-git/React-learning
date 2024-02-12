add5d89c8a8ae1d338302d48d85beae8dffdaaa1
yonghowfung <yhfung940217@gmail.com>
2024-02-12 21:59:18 +0100

    use Modal if empty input

--- section_4/project_management/src/components/NewProject.jsx
+++ section_4/project_management/src/components/NewProject.jsx
@@ -1,11 +1,13 @@
 import { useRef } from "react";
 
 import Input from "./Input";
+import Modal from "./Modal";
 
 const NewProject = ({ onCancel, onAddProject }) => {
   const titleRef = useRef();
   const descriptionRef = useRef();
   const dueDateRef = useRef();
+  const dialog = useRef();
 
   const buttonClasses =
     "px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950";
@@ -15,6 +17,14 @@ const NewProject = ({ onCancel, onAddProject }) => {
     const description = descriptionRef.current.value;
     const dueDate = dueDateRef.current.value;
 
+    if (
+      title.trim() === "" ||
+      description.trim() === "" ||
+      dueDate.trim() === ""
+    ) {
+      return dialog.current.open();
+    }
+
     onAddProject({ title, description, dueDate });
 
     titleRef.current.value = "";
@@ -23,47 +33,56 @@ const NewProject = ({ onCancel, onAddProject }) => {
   };
 
   return (
-    <div className="w-[35rem] my-16 mx-auto">
-      <menu className="flex items-center justify-end gap-4 my-4 rounded">
-        <li>
-          <button
-            className="text-stone-800 hover:text-stone-950"
-            onClick={onCancel}
-          >
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button className={buttonClasses} onClick={handleSave}>
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input
-          type="text"
-          name="title"
-          id="title"
-          label="Title"
-          ref={titleRef}
-        />
-        <Input
-          name="description"
-          id="description"
-          label="Description"
-          rows="3"
-          textarea
-          ref={descriptionRef}
-        />
-        <Input
-          type="date"
-          name="date"
-          id="date"
-          label="Due Date"
-          ref={dueDateRef}
-        />
+    <>
+      <Modal ref={dialog} buttonCaption="Close">
+        <h2 className="font-bold text-stone-700 text-xl my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops...please make sure you provide a valid value for every input
+          field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] my-16 mx-auto">
+        <menu className="flex items-center justify-end gap-4 my-4 rounded">
+          <li>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button className={buttonClasses} onClick={handleSave}>
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input
+            type="text"
+            name="title"
+            id="title"
+            label="Title"
+            ref={titleRef}
+          />
+          <Input
+            name="description"
+            id="description"
+            label="Description"
+            rows="3"
+            textarea
+            ref={descriptionRef}
+          />
+          <Input
+            type="date"
+            name="date"
+            id="date"
+            label="Due Date"
+            ref={dueDateRef}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
