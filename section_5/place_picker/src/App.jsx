86af5f0d916a8c423b8432fd0288084a878a04fb
yonghowfung <yhfung940217@gmail.com>
2024-02-14 14:23:44 +0100

    use useEffect handl side effect and localstorage to store seleted places id

--- section_5/place_picker/src/App.jsx
+++ section_5/place_picker/src/App.jsx
@@ -7,11 +7,16 @@ import { AVAILABLE_PLACES } from "./data.js";
 import { sortPlacesByDistance } from "./loc.js";
 import logoImg from "./assets/logo.png";
 
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map((id) => {
+  return AVAILABLE_PLACES.find((place) => place.id === id);
+});
+
 const App = () => {
   const modal = useRef();
   const selectedPlace = useRef();
   const [sortedPlaces, setSortedPlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -43,6 +48,15 @@ const App = () => {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -50,6 +64,19 @@ const App = () => {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+
+    if (storedIds.indexOf(selectedPlace.current) !== -1) {
+      const updatedIds = storedIds.filter((id) => id !== selectedPlace.current);
+
+      updatedIds.length > 0
+        ? localStorage.setItem(
+            "selectedPlaces",
+            JSON.stringify([...updatedIds])
+          )
+        : localStorage.removeItem("selectedPlaces");
+    }
   }
 
   return (
