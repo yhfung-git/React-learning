0e9adfabcafe44109cd70c9299092528b0e44c01
yonghowfung <yhfung940217@gmail.com>
2024-02-15 22:10:21 +0100

    renamed + moved related logic into this component

--- section_5/quiz_app/src/components/Answer.jsx
+++
@@ -1,9 +0,0 @@
-const Answer = ({ answer, onSelect }) => {
-  return (
-    <li className="answer">
-      <button onClick={() => onSelect(answer)}>{answer}</button>
-    </li>
-  );
-};
-
-export default Answer;
---
+++ section_5/quiz_app/src/components/Answers.jsx
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+
+const Answers = ({ answers, selectedAnswer, answerState, onSelect }) => {
+  const shuffledAnswers = useRef();
+
+  if (!shuffledAnswers.current) {
+    shuffledAnswers.current = [...answers];
+    shuffledAnswers.current.sort(() => Math.random() - 0.5);
+  }
+
+  return (
+    <ul id="answers">
+      {shuffledAnswers.current.map((answer) => {
+        const isSelected = selectedAnswer === answer;
+        const hasAnswer = answerState === "correct" || answerState === "wrong";
+
+        let cssClass = "";
+        if (isSelected) cssClass = "selected";
+        if (hasAnswer && isSelected) cssClass = answerState;
+
+        return (
+          <li key={answer} className="answer">
+            <button className={cssClass} onClick={() => onSelect(answer)}>
+              {answer}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Answers;
