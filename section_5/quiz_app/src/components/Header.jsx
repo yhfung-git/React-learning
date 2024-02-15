bf0c836846b628cad9e139e4492225465b18d7d4
yonghowfung <yhfung940217@gmail.com>
2024-02-15 17:12:17 +0100

    Header component to show title and logo

---
+++ section_5/quiz_app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/quiz-logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Quiz App Logo" />
+      <h1>Quiz</h1>
+    </header>
+  );
+};
+
+export default Header;
