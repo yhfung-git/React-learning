941616abb7cb161295061515747f009de075a787
yonghowfung <yhfung940217@gmail.com>
2024-02-17 18:55:15 +0100

    new project - counter

---
+++ section_6/counter/src/components/Counter/CounterHistory.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+import { log } from "../../log.js";
+
+function HistoryItem({ count }) {
+  log("<HistoryItem /> rendered", 3);
+
+  const [selected, setSelected] = useState(false);
+
+  function handleClick() {
+    setSelected((prevSelected) => !prevSelected);
+  }
+
+  return (
+    <li onClick={handleClick} className={selected ? "selected" : undefined}>
+      {count}
+    </li>
+  );
+}
+
+const CounterHistory = ({ history }) => {
+  log("<CounterHistory /> rendered", 2);
+
+  return (
+    <ol>
+      {history.map((count, index) => (
+        <HistoryItem key={index} count={count} />
+      ))}
+    </ol>
+  );
+};
+
+export default CounterHistory;
