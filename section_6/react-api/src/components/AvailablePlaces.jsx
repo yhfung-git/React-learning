7b5b1384d2bd16fa1031dc2ccf1b81e9d75cb172
yonghowfung <yhfung940217@gmail.com>
2024-02-23 19:03:16 +0100

    added fetch api to get all places

--- section_6/react-api/src/components/AvailablePlaces.jsx
+++ section_6/react-api/src/components/AvailablePlaces.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from "react";
+
 import Places from "./Places.jsx";
 
 const AvailablePlaces = ({ onSelectPlace }) => {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    };
+
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
