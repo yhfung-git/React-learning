a453c0baa531ca623ba6df8ca226516b9736618d
yonghowfung <yhfung940217@gmail.com>
2024-02-23 19:14:54 +0100

    add isLoading state to show fallback text when fetching places data

--- section_6/react-api/src/components/AvailablePlaces.jsx
+++ section_6/react-api/src/components/AvailablePlaces.jsx
@@ -4,12 +4,15 @@ import Places from "./Places.jsx";
 
 const AvailablePlaces = ({ onSelectPlace }) => {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchPlaces = async () => {
+      setIsLoading(true);
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsLoading(false);
     };
 
     fetchPlaces();
@@ -19,6 +22,8 @@ const AvailablePlaces = ({ onSelectPlace }) => {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Fetching places data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
