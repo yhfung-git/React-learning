2920e811f912a3a7b2bfea31427df057434dd226
yonghowfung <yhfung940217@gmail.com>
2024-02-24 16:06:20 +0100

    changed import function

--- section_6/react-api/src/components/AvailablePlaces.jsx
+++ section_6/react-api/src/components/AvailablePlaces.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
-import fetchAvailablePlaces from "../http.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 const AvailablePlaces = ({ onSelectPlace }) => {
   const [availablePlaces, setAvailablePlaces] = useState([]);
