849391cd7eadd50cc43291632ab8741c9b45d04e
yonghowfung <yhfung940217@gmail.com>
2024-02-24 16:46:13 +0100

    added updateUserPlaces function

--- section_6/react-api/src/http.js
+++ section_6/react-api/src/http.js
@@ -1,4 +1,4 @@
-const fetchAvailablePlaces = async () => {
+export const fetchAvailablePlaces = async () => {
   const response = await fetch("http://localhost:3000/places");
   const resData = await response.json();
 
@@ -9,4 +9,18 @@ const fetchAvailablePlaces = async () => {
   return resData.places;
 };
 
-export default fetchAvailablePlaces;
+export const updateUserPlaces = async (places) => {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ places }),
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user places");
+  }
+
+  return resData.message;
+};
