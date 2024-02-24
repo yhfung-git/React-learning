fc9772fb10d1a5098e6e099e80aebd5cf59a0916
yonghowfung <yhfung940217@gmail.com>
2024-02-24 14:50:44 +0100

    adjusted main file name and added app.listen callback to log some infos

--- section_6/react-api/backend/app.js
+++ section_6/react-api/backend/app.js
@@ -50,4 +50,6 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "404 - Not Found" });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
