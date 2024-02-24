dd9abe6b56b18de9fcd5e9621b15c2a2e84e3dd4
yonghowfung <yhfung940217@gmail.com>
2024-02-24 15:13:11 +0100

    use try catch and Error component to  handle errors

--- section_6/react-api/src/components/AvailablePlaces.jsx
+++ section_6/react-api/src/components/AvailablePlaces.jsx
@@ -1,22 +1,43 @@
 import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 const AvailablePlaces = ({ onSelectPlace }) => {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchPlaces = async () => {
       setIsLoading(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      }
+
       setIsLoading(false);
     };
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    const errroMessage =
+      error.message || "Could not fetch places, please try again later.";
+
+    return <Error title="An error occurred!" message={errroMessage} />;
+  }
 
   return (
     <Places
